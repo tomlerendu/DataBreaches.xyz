2896efd9b9d1f9e293798c7052d16dfff111dc0c
Tom Lerendu <tom.lerendu@gmail.com>
2016-06-28 15:39:30 +0100

    Use environment variables for mail config

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => env('APP_EMAIL'), 'name' => env('APP_NAME')],
 
     /*
     |--------------------------------------------------------------------------
